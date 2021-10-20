cc2fe5902d76e8e2f56ce3ce1cc13f9e01bbaf32
mark <markpackham1@gmail.com>
2021-10-20 14:52:17 +0100

    correct file name for storing passwords

--- utils/savePassword.js
+++ utils/savePassword.js
@@ -4,7 +4,7 @@ const os = require("os");
 const chalk = require("chalk");
 
 const savePassword = (password) => {
-  fs.open(path.join(__dirname, "../", "password.txt"), "a", 666, (e, id) => {
+  fs.open(path.join(__dirname, "../", "passwords.txt"), "a", 666, (e, id) => {
     // EOL means end of line
     fs.write(id, password + os.EOL, null, "utf-8", () => {
       fs.close(id, () => {
