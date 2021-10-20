50fcd8dc2e2010defafec559cfdcd0116ac93733
mark <markpackham1@gmail.com>
2021-10-20 13:36:25 +0100

    add createPassord file
    https://youtu.be/3Xx83JAktXk?t=1068

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const log = console.log;
 
 program.version("1.0.0").description("Password Generator");
 
-program.option("-l, --length <number>", "length of password").parse();
+// 8 is set as a default length
+program
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "forbid numbers")
+  .option("-ns, --no-symbols", "forbid symbols")
+  .parse();
 
-log(program.opts());
+const { length, save, numbers, symbols } = program.opts();
+
+// Get generated password
+const generatedPassword = createPassword(length, numbers, symbols);
+
+// Output generated password
+log(generatedPassword);
